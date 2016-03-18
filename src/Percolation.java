2a1c08f0b107bb13dc7ffe6d9972d3ad1e7c6393
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-03-18 14:28:56 +0900

    implement Percolation

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,52 +1,86 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 import java.util.stream.IntStream;
 
 public class Percolation {
 	private int N;
+	private int[] filter;
 	private int virtualTopIndex;
 	private int virtualBottomIndex;
 
-	private WeightedQuickUnionUF filter;
+	private WeightedQuickUnionUF weightedQuickUnionUF;
 
 	// create N-by-N grid, with all sites blocked
 	public Percolation(int N) {
-		filter = new WeightedQuickUnionUF(N * N + 2);
+		this.N = N;
+		filter = new int[N * N];
+
+		weightedQuickUnionUF = new WeightedQuickUnionUF(N * N + 2);
 		virtualTopIndex = N * N;
 		virtualBottomIndex = N * N + 1;
-		// connect first row to virtaul top
-		IntStream.range(0, N).forEach(i -> filter.union(virtualTopIndex, i));
+		// connect first row to virtual top
+		IntStream.range(0, N).forEach(i -> weightedQuickUnionUF.union(virtualTopIndex, i));
 
 		// connect first row to virtual bottom
-		IntStream.range(N * (N - 1), N * N).forEach(i -> filter.union(virtualBottomIndex, i));
-
+		IntStream.range(N * (N - 1), N * N).forEach(i -> weightedQuickUnionUF.union(virtualBottomIndex, i));
 	}
 
 	// open site (row i, column j) if it is not open already
 	public void open(int i, int j) {
-
+		int index = i * N + j;
+		filter[index] = 1;
+		if (j < N - 1 && isOpen(i, j + 1))
+			weightedQuickUnionUF.union(index, index + 1);
+		if (j > 0 && isOpen(i, j - 1))
+			weightedQuickUnionUF.union(index, index - 1);
+		if (i < N - 1 && isOpen(i + 1, j))
+			weightedQuickUnionUF.union(index, index + N);
+		if (i > 0 && isOpen(i - 1, j))
+			weightedQuickUnionUF.union(index, index - N);
 	}
 
 	// is site (row i, column j) open?
 	public boolean isOpen(int i, int j) {
-		return false;
+		return filter[i * N + j] == 1;
 	}
 
 	// is site (row i, column j) full?
+	// A full site is an open site that can be connected to an open site -
+	// in the top row via a chain of neighboring (left, right, up, down) open sites.
 	public boolean isFull(int i, int j) {
-		return false;
+		return isOpen(i, j)
+				&& j < N - 1 && isOpen(i, j + 1)
+				&& j > 0 && isOpen(i, j - 1)
+				&& i < N - 1 && isOpen(i + 1, j)
+				&& i > 0 && isOpen(i - 1, j);
 	}
 
 	// does the system percolate?
 	public boolean percolates() {
-		return false;
+		return weightedQuickUnionUF.connected(virtualTopIndex, virtualBottomIndex);
 	}
 
 	// test client (optional)
 	public static void main(String[] args) {
 		int N = 5;
 		Percolation percolation = new Percolation(N);
-		percolation.percolates();
 
+		int opened = 0;
+		while (!percolation.percolates()) {
+			int i = StdRandom.uniform(0, N);
+			int j = StdRandom.uniform(0, N);
+
+			StdOut.println(i + "," + j);
+
+			if (percolation.isOpen(i, j))
+				continue;
+
+			percolation.open(i, j);
+			opened++;
+		}
+
+		StdOut.println(N * N + " : " + opened);
 	}
 }
