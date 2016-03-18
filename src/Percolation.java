2111f367d4a64f569859bc3092156dcb6b52ec61
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-03-18 11:09:57 +0900

    implement constructor and related test code

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,10 +1,21 @@
+import java.util.stream.IntStream;
+
 public class Percolation {
+	int N;
+	int[][] filter;
+	int top = -1;
+	int bottom = -2;
+
 	// create N-by-N grid, with all sites blocked
 	public Percolation(int N) {
+		this.N = N;
+		filter = new int[N][N];
+		IntStream.range(0, N).forEach(i -> IntStream.range(0, N).forEach(j -> filter[i][j] = i * N + j));
 	}
 
 	// open site (row i, column j) if it is not open already
 	public void open(int i, int j) {
+
 	}
 
 	// is site (row i, column j) open?
@@ -22,7 +33,16 @@ public class Percolation {
 		return false;
 	}
 
+	public void print() {
+		IntStream.range(0, N).forEach(
+				i -> IntStream.range(0, N).forEach(j -> System.out.println(i + "," + j + " = " + filter[i][j])));
+	}
+
 	// test client (optional)
 	public static void main(String[] args) {
+		int N = 5;
+		Percolation percolation = new Percolation(N);
+		percolation.print();
+
 	}
 }
