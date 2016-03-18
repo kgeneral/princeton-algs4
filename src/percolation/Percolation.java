f719587be062f320208451866094f5bf34093fd9
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-03-18 15:45:59 +0900

    change indenting strategy (tab -> space)

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -3,93 +3,93 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-	private int N;
-	private int[] filter;
-	private int virtualTopIndex;
-	private int virtualBottomIndex;
+    private int N;
+    private int[] filter;
+    private int virtualTopIndex;
+    private int virtualBottomIndex;
 
-	private WeightedQuickUnionUF unionFindForUpwardConnectivity;
-	private WeightedQuickUnionUF unionFindForGlobal;
+    private WeightedQuickUnionUF unionFindForUpwardConnectivity;
+    private WeightedQuickUnionUF unionFindForGlobal;
 
-	// create N-by-N grid, with all sites blocked
-	public Percolation(int N) {
-		if (N < 1)
-			throw new IllegalArgumentException();
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+        if (N < 1)
+            throw new IllegalArgumentException();
 
-		this.N = N;
-		filter = new int[N * N];
+        this.N = N;
+        filter = new int[N * N];
 
-		unionFindForGlobal = new WeightedQuickUnionUF(N * N + 2);
-		unionFindForUpwardConnectivity = new WeightedQuickUnionUF(N * N + 1);
-		virtualTopIndex = N * N;
-		virtualBottomIndex = N * N + 1;
-		// connect first row to virtual top
-		for (int i = 0; i < N; i++)
-			union(virtualTopIndex, i);
+        unionFindForGlobal = new WeightedQuickUnionUF(N * N + 2);
+        unionFindForUpwardConnectivity = new WeightedQuickUnionUF(N * N + 1);
+        virtualTopIndex = N * N;
+        virtualBottomIndex = N * N + 1;
+        // connect first row to virtual top
+        for (int i = 0; i < N; i++)
+            union(virtualTopIndex, i);
 
-		// connect first row to virtual bottom
-		for (int i = N * (N - 1); i < N * N; i++)
-			unionFindForGlobal.union(virtualBottomIndex, i);
-	}
+        // connect first row to virtual bottom
+        for (int i = N * (N - 1); i < N * N; i++)
+            unionFindForGlobal.union(virtualBottomIndex, i);
+    }
 
-	// open site (row i, column j) if it is not open already
-	public void open(int i, int j) {
-		int index = (i - 1) * N + j - 1;
-		filter[index] = 1;
-		if (j < N && isOpen(i, j + 1))
-			union(index, index + 1);
-		if (j > 1 && isOpen(i, j - 1))
-			union(index, index - 1);
-		if (i < N && isOpen(i + 1, j))
-			union(index, index + N);
-		if (i > 1 && isOpen(i - 1, j))
-			union(index, index - N);
-	}
+    // open site (row i, column j) if it is not open already
+    public void open(int i, int j) {
+        int index = (i - 1) * N + j - 1;
+        filter[index] = 1;
+        if (j < N && isOpen(i, j + 1))
+            union(index, index + 1);
+        if (j > 1 && isOpen(i, j - 1))
+            union(index, index - 1);
+        if (i < N && isOpen(i + 1, j))
+            union(index, index + N);
+        if (i > 1 && isOpen(i - 1, j))
+            union(index, index - N);
+    }
 
-	private void union(int p, int q) {
-		unionFindForGlobal.union(p, q);
-		unionFindForUpwardConnectivity.union(p, q);
-	}
+    private void union(int p, int q) {
+        unionFindForGlobal.union(p, q);
+        unionFindForUpwardConnectivity.union(p, q);
+    }
 
-	// is site (row i, column j) open?
-	public boolean isOpen(int i, int j) {
-		int index = (i - 1) * N + j - 1;
-		if (index < 0 || index >= N * N)
-			throw new IndexOutOfBoundsException();
-		return filter[index] == 1;
-	}
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        int index = (i - 1) * N + j - 1;
+        if (index < 0 || index >= N * N)
+            throw new IndexOutOfBoundsException();
+        return filter[index] == 1;
+    }
 
-	// is site (row i, column j) full? - opened and connected with top
-	public boolean isFull(int i, int j) {
-		int index = (i - 1) * N + j - 1;
-		return isOpen(i, j) && unionFindForGlobal.connected(index, virtualTopIndex)
-				&& unionFindForUpwardConnectivity.connected(index, virtualTopIndex);
-	}
+    // is site (row i, column j) full? - opened and connected with top
+    public boolean isFull(int i, int j) {
+        int index = (i - 1) * N + j - 1;
+        return isOpen(i, j) && unionFindForGlobal.connected(index, virtualTopIndex)
+                && unionFindForUpwardConnectivity.connected(index, virtualTopIndex);
+    }
 
-	// does the system percolate?
-	public boolean percolates() {
-		return unionFindForGlobal.connected(virtualTopIndex, virtualBottomIndex);
-	}
+    // does the system percolate?
+    public boolean percolates() {
+        return unionFindForGlobal.connected(virtualTopIndex, virtualBottomIndex);
+    }
 
-	// test client (optional)
-	public static void main(String[] args) {
-		int N = 5;
-		Percolation percolation = new Percolation(N);
+    // test client (optional)
+    public static void main(String[] args) {
+        int N = 5;
+        Percolation percolation = new Percolation(N);
 
-		int opened = 0;
-		while (!percolation.percolates()) {
-			int i = StdRandom.uniform(1, N + 1);
-			int j = StdRandom.uniform(1, N + 1);
+        int opened = 0;
+        while (!percolation.percolates()) {
+            int i = StdRandom.uniform(1, N + 1);
+            int j = StdRandom.uniform(1, N + 1);
 
-			StdOut.println(i + "," + j);
+            StdOut.println(i + "," + j);
 
-			if (percolation.isOpen(i, j))
-				continue;
+            if (percolation.isOpen(i, j))
+                continue;
 
-			percolation.open(i, j);
-			opened++;
-		}
+            percolation.open(i, j);
+            opened++;
+        }
 
-		StdOut.println(N * N + " : " + opened);
-	}
+        StdOut.println(N * N + " : " + opened);
+    }
 }
--- src/percolation/PercolationStats.java
+++ src/percolation/PercolationStats.java
@@ -5,67 +5,67 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-	private int N;
-	private int T;
-	private Percolation percolation;
+    private int N;
+    private int T;
+    private Percolation percolation;
 
-	private double[] simulatedRatios;
+    private double[] simulatedRatios;
 
-	// perform T independent experiments on an N-by-N grid
-	public PercolationStats(int N, int T) {
-		this.N = N;
-		this.T = T;
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        this.N = N;
+        this.T = T;
 
-		simulatedRatios = new double[T];
+        simulatedRatios = new double[T];
 
-		for (int i = 0; i < T; i++)
-			simulatedRatios[i] = (double) simulate(N) / (N * N);
-	}
+        for (int i = 0; i < T; i++)
+            simulatedRatios[i] = (double) simulate(N) / (N * N);
+    }
 
-	// sample mean of percolation threshold
-	public double mean() {
-		return StdStats.mean(simulatedRatios);
-	}
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(simulatedRatios);
+    }
 
-	// sample standard deviation of percolation threshold
-	public double stddev() {
-		return StdStats.stddev(simulatedRatios);
-	}
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(simulatedRatios);
+    }
 
-	// low endpoint of 95% confidence interval
-	public double confidenceLo() {
-		return mean() - 1.96 * stddev() / Math.sqrt(T);
-	}
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
 
-	// high endpoint of 95% confidence interval
-	public double confidenceHi() {
-		return mean() + 1.96 * stddev() / Math.sqrt(T);
-	}
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 
-	private int simulate(int N) {
-		int opened = 0;
-		percolation = new Percolation(N);
-		while (!percolation.percolates()) {
-			int i = StdRandom.uniform(1, N + 1);
-			int j = StdRandom.uniform(1, N + 1);
-			if (percolation.isOpen(i, j))
-				continue;
-			percolation.open(i, j);
-			opened++;
-		}
-		return opened;
-	}
+    private int simulate(int N) {
+        int opened = 0;
+        percolation = new Percolation(N);
+        while (!percolation.percolates()) {
+            int i = StdRandom.uniform(1, N + 1);
+            int j = StdRandom.uniform(1, N + 1);
+            if (percolation.isOpen(i, j))
+                continue;
+            percolation.open(i, j);
+            opened++;
+        }
+        return opened;
+    }
 
-	// test client (described below)
-	public static void main(String[] args) {
-		int N = StdIn.readInt();
-		int T = StdIn.readInt();
+    // test client (described below)
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        int T = StdIn.readInt();
 
-		PercolationStats percolationStats = new PercolationStats(N, T);
+        PercolationStats percolationStats = new PercolationStats(N, T);
 
-		StdOut.println("mean = " + percolationStats.mean());
-		StdOut.println("stddev = " + percolationStats.stddev());
-		StdOut.println("95% confidence interval = " + percolationStats.confidenceLo() + ", "
-				+ percolationStats.confidenceHi());
-	}
+        StdOut.println("mean = " + percolationStats.mean());
+        StdOut.println("stddev = " + percolationStats.stddev());
+        StdOut.println("95% confidence interval = " + percolationStats.confidenceLo() + ", "
+                + percolationStats.confidenceHi());
+    }
 }
