5770afc1d7b2254187536894a274d8ff2e8a50aa
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-03-22 14:57:24 +0900

    implement RandomizedQueue, related test code

--- src/queues/RandomizedQueue.java
+++ src/queues/RandomizedQueue.java
@@ -1,40 +1,153 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    private static final int BASE_SIZE = 10;
+    private Item[] data;
+    private int size;
+    private int cursor;
+
     // construct an empty randomized queue
     public RandomizedQueue() {
+        data = (Item[]) new Object[BASE_SIZE];
+        size = 0;
+        cursor = 0;
     }
 
     // is the queue empty?
     public boolean isEmpty() {
-        return false;
+        return size() <= 0;
     }
 
     // return the number of items on the queue
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the item
     public void enqueue(Item item) {
+        data[cursor++] = item;
+        size++;
+        if (cursor >= data.length)
+            resize(data.length * 2);
+    }
+
+    private void resize(int targetSize) {
+        // no more shrink under BASE_SIZE
+        if (Math.max(data.length, targetSize) <= BASE_SIZE)
+            return;
+
+        Item[] resizedData = (Item[]) new Object[targetSize];
+        int movableSize = Math.min(data.length, targetSize);
+        int idx = 0;
+        for (int i = 0; i < movableSize; i++) {
+            if (data[i] == null)
+                continue;
+
+            resizedData[idx++] = data[i];
+        }
+        size = idx;
+        data = resizedData;
     }
 
     // remove and return a random item
     public Item dequeue() {
-        return null;
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        int randomIndex = getRandomIndex();
+        Item removed = data[randomIndex];
+        data[randomIndex] = null;
+        size--;
+        return removed;
+    }
+
+    private int getRandomIndex() {
+        do {
+            int randomIndex = StdRandom.uniform(data.length);
+            if (data[randomIndex] != null)
+                return randomIndex;
+        } while (true);
     }
 
     // return (but do not remove) a random item
     public Item sample() {
-        return null;
+        if (isEmpty())
+            throw new NoSuchElementException();
+        int randomIndex = getRandomIndex();
+        return data[randomIndex];
     }
+
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-        return null;
+        return new Itr();
+    }
+
+    private class Itr implements Iterator<Item> {
+
+        private int cursor = 0;
+        private final int currentSize = size;
+        private boolean[] iterated = new boolean[data.length];
+
+        @Override
+        public boolean hasNext() {
+            return cursor < currentSize;
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+
+            int randomIndex = getRandomIndex();
+            Item next = data[randomIndex];
+            cursor++;
+            iterated[randomIndex] = true;
+            return next;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        private int getRandomIndex() {
+            do {
+                int randomIndex = StdRandom.uniform(data.length);
+                if (iterated[randomIndex])
+                    continue;
+                if (data[randomIndex] == null)
+                    continue;
+
+                return randomIndex;
+            } while (true);
+        }
     }
 
     // unit testing
     public static void main(String[] args) {
     }
+
+    //    private int[] shuffle(int size) {
+//        int[] shuffle = new int[size];
+//        for (int i = 0; i < size; i++)
+//            shuffle[i] = i;
+//
+//        for (int i = 0; i < size - 1; i++) {
+//            if (StdRandom.uniform(2) == 0)
+//                swapIndex(i, i + 1);
+//        }
+//
+//        return shuffle;
+//    }
+//
+//    private void swapIndex(int left, int right) {
+//        Item temp = data[left];
+//        data[left] = data[right];
+//        data[right] = temp;
+//    }
 }
--- src/queues/UnitTests.java
+++ src/queues/UnitTests.java
@@ -8,33 +8,67 @@ public class UnitTests {
     // unit testing
     public static void main(String[] args) {
 
-        int N = StdRandom.uniform(2000, 3000);
+        int N = StdRandom.uniform(100, 300);
         StdOut.println("N : " + N);
 
-        Deque<Integer> deque = new Deque<>();
+//        Deque<Integer> deque = new Deque<>();
+//        IntStream.range(0, N).sequential().forEach(i -> {
+//            int val = StdRandom.uniform(1, N);
+//            int operationCase = val % 4;
+//            try {
+//                if (operationCase == 0)
+//                    deque.addFirst(val);
+//                if (operationCase == 1)
+//                    deque.addLast(val);
+//                if (operationCase == 2)
+//                    deque.removeFirst();
+//                if (operationCase == 3)
+//                    deque.removeLast();
+//            } catch (NoSuchElementException e) {
+//
+//            } catch (ArrayIndexOutOfBoundsException e) {
+//                StdOut.println("operationCase : " + operationCase);
+//                StdOut.println("ArrayIndexOutOfBoundsException");
+//                //deque.print();
+//                e.printStackTrace();
+//                throw new RuntimeException();
+//            }
+//        });
+        //deque.print();
+
+        RandomizedQueue<Integer> randomizedQueue = new RandomizedQueue<>();
+
         IntStream.range(0, N).sequential().forEach(i -> {
             int val = StdRandom.uniform(1, N);
-            int operationCase = val % 4;
+            int operationCase = val % 10;
             try {
-                if (operationCase == 0)
-                    deque.addFirst(val);
-                if (operationCase == 1)
-                    deque.addLast(val);
-                if (operationCase == 2)
-                    deque.removeFirst();
-                if (operationCase == 3)
-                    deque.removeLast();
-            } catch (NoSuchElementException e) {
+                int removed = -1;
+                if (operationCase >= 5)
+                    randomizedQueue.enqueue(val);
+                else
+                    removed = randomizedQueue.dequeue();
 
+                StdOut.println((operationCase >= 5) ? "enqueue " + val : "dequeue " + removed);
+
+                //print(randomizedQueue);
+                for (Integer item : randomizedQueue) {
+                    StdOut.print(item + ", ");
+                }
+                StdOut.println();
+
+            } catch (NoSuchElementException e) {
             } catch (ArrayIndexOutOfBoundsException e) {
-                StdOut.println("operationCase : " + operationCase);
-                StdOut.println("ArrayIndexOutOfBoundsException");
-                //deque.print();
                 e.printStackTrace();
-                throw new RuntimeException();
             }
         });
-        //deque.print();
     }
+
+    private static <T> void print(Iterable<T> iterable) {
+        for (T item : iterable) {
+            StdOut.print(item.toString() + ", ");
+        }
+        StdOut.println();
+    }
+
 
 }
