89810fa4ada07edc87ba8639921b762728d52dfc
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-03-25 18:57:43 +0900

    update test code

--- src/collinear/SampleClient.java
+++ src/collinear/SampleClient.java
@@ -25,7 +25,13 @@ public class SampleClient {
         StdDraw.show();
 
         // print and draw the line segments
-        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+//        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+//        for (LineSegment segment : collinear.segments()) {
+//            StdOut.println(segment);
+//            segment.draw();
+//        }
+
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
         for (LineSegment segment : collinear.segments()) {
             StdOut.println(segment);
             segment.draw();
