b89b224d41b31ef9a8a27ac63daf90a5f386bd50
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-03-30 18:44:06 +0900

    add basic implements

--- src/kdtree/PointSET.java
+++ src/kdtree/PointSET.java
@@ -1,32 +1,41 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
 
 public class PointSET {
+
+    private SET<Point2D> points;
+
     // construct an empty set of points
     public PointSET() {
+        points = new SET<>();
+
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return points.isEmpty();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return points.size();
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        points.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return false;
+        return points.contains(p);
     }
 
     // draw all points to standard draw
     public void draw() {
+        for(Point2D point : points)
+            point.draw();
     }
 
     // all points that are inside the rectangle
