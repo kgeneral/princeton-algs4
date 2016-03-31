4c625c7f660f12e6c0bd1a9d38dee4924871e54d
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-03-31 19:02:35 +0900

    implement functions

--- src/kdtree/KdTree.java
+++ src/kdtree/KdTree.java
@@ -1,16 +1,64 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
 
+    private static final RectHV initialBoundary = new RectHV(0, 0, 1, 1);
+
     private static class Node {
         private Point2D p;      // the point
-        private RectHV rect;    // the axis-aligned rectangle corresponding to this node
+        private RectHV rect = initialBoundary;    // the axis-aligned rectangle corresponding to this node
         private Node lb;        // the left/bottom subtree
         private Node rt;        // the right/top subtree
+        private boolean isHorizonalSplit = true;
+        private boolean isComparedYCoordFirst = false;
 
         public Node(Point2D p) {
+            this(p, null);
+        }
+
+        public Node(Point2D p, Node parent) {
             this.p = p;
+            if (parent == null) return;
+
+            isHorizonalSplit = !parent.isHorizonalSplit;
+            isComparedYCoordFirst = !parent.isComparedYCoordFirst;
+            RectHV parentBoundary = parent.rect;
+
+            if (parent.compareTo(p) > 0)
+                this.rect = (isHorizonalSplit) ?
+                        new RectHV(parentBoundary.xmin(), parentBoundary.ymin(), parentBoundary.xmax(), parent.p.y()) :
+                        new RectHV(parentBoundary.xmin(), parentBoundary.ymin(), parent.p.x(), parentBoundary.ymax());
+            else
+                this.rect = (isHorizonalSplit) ?
+                        new RectHV(parentBoundary.xmin(), parent.p.y(), parentBoundary.xmax(), parentBoundary.ymax()) :
+                        new RectHV(parent.p.x(), parentBoundary.ymin(), parentBoundary.xmax(), parentBoundary.ymax());
+
+        }
+
+        public int compareTo(Point2D that) {
+            if (isComparedYCoordFirst)
+                return compareYCoordFirst(that);
+
+            return compareXCoordFirst(that);
+        }
+
+        private int compareYCoordFirst(Point2D that) {
+            if (this.p.y() < that.y()) return -1;
+            if (this.p.y() > that.y()) return +1;
+            if (this.p.x() < that.x()) return -1;
+            if (this.p.x() > that.x()) return +1;
+            return 0;
+        }
+
+        private int compareXCoordFirst(Point2D that) {
+            if (this.p.x() < that.x()) return -1;
+            if (this.p.x() > that.x()) return +1;
+            if (this.p.y() < that.y()) return -1;
+            if (this.p.y() > that.y()) return +1;
+            return 0;
         }
     }
 
@@ -39,21 +87,20 @@ public class KdTree {
             root = new Node(p);
             return;
         }
-        Node newNode = new Node(p);
         Node cursor = root;
         Node prev = root;
         while (cursor != null) {
             prev = cursor;
-            if (cursor.p.compareTo(p) > 0)
+            if (cursor.compareTo(p) > 0)
                 cursor = cursor.lb;
             else
                 cursor = cursor.rt;
         }
 
-        if (prev.p.compareTo(p) > 0)
-            prev.lb = newNode;
+        if (prev.compareTo(p) > 0)
+            prev.lb = new Node(p, prev);
         else
-            prev.rt = newNode;
+            prev.rt = new Node(p, prev);
         N++;
     }
 
@@ -61,10 +108,10 @@ public class KdTree {
     public boolean contains(Point2D p) {
         Node traverse = root;
         while (traverse != null) {
-            if (traverse.p.compareTo(p) == 0)
+            if (traverse.compareTo(p) == 0)
                 return true;
 
-            if (traverse.p.compareTo(p) > 0)
+            if (traverse.compareTo(p) > 0)
                 traverse = traverse.lb;
             else
                 traverse = traverse.rt;
@@ -81,19 +128,64 @@ public class KdTree {
     // DFS preorder
     private void draw(Node node) {
         if (node == null) return;
+        StdDraw.setPenRadius(.01);
+        StdDraw.setPenColor(StdDraw.BLACK);
         node.p.draw();
+        StdDraw.setPenRadius(.001);
+        StdDraw.setPenColor(StdDraw.BLUE);
+        if (node.isHorizonalSplit)
+            StdDraw.setPenColor(StdDraw.RED);
+        node.rect.draw();
+
         draw(node.lb);
         draw(node.rt);
     }
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        Queue<Point2D> queue = new Queue<>();
+        range(queue, rect, root);
+        return queue;
+    }
+
+    private void range(Queue<Point2D> queue, RectHV rect, Node node) {
+        if (node == null) return;
+        if (rect.intersects(node.rect)) {
+            if (rect.contains(node.p))
+                queue.enqueue(node.p);
+            range(queue, rect, node.lb);
+            range(queue, rect, node.rt);
+        }
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return p;
+        Point2D closest = new Point2D(Double.MAX_VALUE, Double.MAX_VALUE);
+        closest = nearest(p, closest, root);
+        return closest;
+    }
+
+    // if the closest point discovered so far is closer than the distance between the query point and the rectangle corresponding to a node,
+    // there is no need to explore that node (or its subtrees).
+    private Point2D nearest(Point2D query, Point2D closest, Node node) {
+        if (node == null) return closest;
+        if(node.rect.distanceTo(query) < closest.distanceTo(query)) {
+            Point2D currentClosest = closest;
+            if(currentClosest.distanceTo(query) > node.p.distanceTo(query))
+                currentClosest = node.p;
+
+            Point2D nearestLeft = nearest(query, closest, node.lb);
+            if(currentClosest.distanceTo(query) > nearestLeft.distanceTo(query))
+                currentClosest = nearestLeft;
+
+            Point2D nearestRight = nearest(query, closest, node.rt);
+            if(currentClosest.distanceTo(query) > nearestRight.distanceTo(query))
+                currentClosest = node.p;
+
+            return currentClosest;
+        }
+
+        return closest;
     }
 
     // unit testing of the methods (optional)
