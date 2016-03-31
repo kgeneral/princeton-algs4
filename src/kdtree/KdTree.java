4d5bfc74a9c61a54d3d5992d98ab153b2fa3dcc5
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-03-31 12:35:24 +0900

    implement preordered dos and relatives

--- src/kdtree/KdTree.java
+++ src/kdtree/KdTree.java
@@ -1,5 +1,3 @@
-import edu.princeton.cs.algs4.BST;
-import edu.princeton.cs.algs4.BinarySearchST;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
@@ -22,8 +20,6 @@ public class KdTree {
     // construct an empty set of points
     public KdTree() {
         N = 0;
-        Node root;
-
     }
 
     // is the set empty?
@@ -38,23 +34,56 @@ public class KdTree {
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (contains(p)) return;
+        if (root == null) {
+            root = new Node(p);
+            return;
+        }
         Node newNode = new Node(p);
-        Node traverse = root;
-        while(traverse != null)
-            traverse = traverse.lb;
+        Node cursor = root;
+        Node prev = root;
+        while (cursor != null) {
+            prev = cursor;
+            if (cursor.p.compareTo(p) > 0)
+                cursor = cursor.lb;
+            else
+                cursor = cursor.rt;
+        }
 
-        traverse = newNode;
+        if (prev.p.compareTo(p) > 0)
+            prev.lb = newNode;
+        else
+            prev.rt = newNode;
         N++;
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
+        Node traverse = root;
+        while (traverse != null) {
+            if (traverse.p.compareTo(p) == 0)
+                return true;
+
+            if (traverse.p.compareTo(p) > 0)
+                traverse = traverse.lb;
+            else
+                traverse = traverse.rt;
+        }
+
         return false;
     }
 
     // draw all points to standard draw
     public void draw() {
+        draw(root);
+    }
 
+    // DFS preorder
+    private void draw(Node node) {
+        if (node == null) return;
+        node.p.draw();
+        draw(node.lb);
+        draw(node.rt);
     }
 
     // all points that are inside the rectangle
