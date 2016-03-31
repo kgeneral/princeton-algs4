72f9a2a8739a231415e1d132763cd08d7740cf62
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-03-31 13:44:25 +0900

    refactor code

--- src/kdtree/PointSET.java
+++ src/kdtree/PointSET.java
@@ -40,21 +40,18 @@ public class PointSET {
     public Iterable<Point2D> range(RectHV rect) {
         Queue<Point2D> ranged = new Queue<>();
         for (Point2D point : points)
-            if (rect.xmin() <= point.x())
-                if (rect.xmax() >= point.x())
-                    if (rect.ymin() <= point.y())
-                        if (rect.ymax() >= point.y())
-                            ranged.enqueue(point);
+            if (rect.contains(point))
+                ranged.enqueue(point);
         return ranged;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
         MinPQ<Point2D> nearest = new MinPQ<>((p1, p2) -> {
-            if(p.distanceTo(p1) == p.distanceTo(p2))
+            if (p.distanceTo(p1) == p.distanceTo(p2))
                 return 0;
 
-            return (p.distanceTo(p1) < p.distanceTo(p2))? -1 : 1;
+            return (p.distanceTo(p1) < p.distanceTo(p2)) ? -1 : 1;
         });
         for (Point2D point : points)
             nearest.insert(point);
