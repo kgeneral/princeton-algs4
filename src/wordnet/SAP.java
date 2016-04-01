32adaab48996acffab547629a0664fe89b397e8e
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-04-01 19:02:22 +0900

    implement ancestor

--- src/wordnet/SAP.java
+++ src/wordnet/SAP.java
@@ -1,19 +1,56 @@
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.SET;
 
 public class SAP {
 
+    private Digraph source;
+    private BreadthFirstDirectedPaths[] shortenPathes;
+
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
+        source = G;
+        shortenPathes = new BreadthFirstDirectedPaths[source.V()];
+        for (int v = 0; v < source.V(); v++) {
+            shortenPathes[v] = new BreadthFirstDirectedPaths(source, v);
+
+
+        }
+        //
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        return v;
+        return 0;
+    }
+
+    private int root(int v) {
+
+        return 0;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        return v;
+        // find all ancestors of x and y
+        Iterable<Integer> ancestorsV = ancestors(v);
+        Iterable<Integer> ancestorsW = ancestors(w);
+        // then return the maximum element of the intersection of the two sets
+        int result = -1;
+        for (int ancestorV : ancestorsV)
+            for (int ancestorW : ancestorsW)
+                if (ancestorV == ancestorW)
+                    if (result < ancestorV)
+                        result = ancestorV;
+
+        return result;
+    }
+
+    private Iterable<Integer> ancestors(int w) {
+        SET<Integer> ancestors = new SET<>();
+        for (int v : source.adj(w))
+            ancestors.add(v);
+
+        return ancestors;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
