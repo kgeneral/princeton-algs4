64fcb7b438d8f7b81f263fb653abe18cb702c4b6
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-04-04 21:45:32 +0900

    implement ancestor

--- src/wordnet/SAP.java
+++ src/wordnet/SAP.java
@@ -13,20 +13,14 @@ public class SAP {
         shortenPathes = new BreadthFirstDirectedPaths[source.V()];
         for (int v = 0; v < source.V(); v++) {
             shortenPathes[v] = new BreadthFirstDirectedPaths(source, v);
-
-
         }
-        //
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        return 0;
-    }
-
-    private int root(int v) {
-
-        return 0;
+        int commonAncestor = ancestor(v, w);
+        if (commonAncestor < 0) return -1;
+        return shortenPathes[v].distTo(commonAncestor) + shortenPathes[w].distTo(commonAncestor);
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
@@ -36,31 +30,67 @@ public class SAP {
         Iterable<Integer> ancestorsW = ancestors(w);
         // then return the maximum element of the intersection of the two sets
         int result = -1;
+        SET<Integer> commonAncestors = new SET<>();
         for (int ancestorV : ancestorsV)
             for (int ancestorW : ancestorsW)
                 if (ancestorV == ancestorW)
-                    if (result < ancestorV)
-                        result = ancestorV;
+                    commonAncestors.add(ancestorV);
+
+        int dist = Integer.MAX_VALUE;
+        for (int candidate : commonAncestors)
+            if (dist > shortenPathes[v].distTo(candidate) + shortenPathes[w].distTo(candidate)) {
+                dist = shortenPathes[v].distTo(candidate) + shortenPathes[w].distTo(candidate);
+                result = candidate;
+            }
 
         return result;
     }
 
-    private Iterable<Integer> ancestors(int w) {
+    private SET<Integer> ancestors(int v) {
         SET<Integer> ancestors = new SET<>();
-        for (int v : source.adj(w))
-            ancestors.add(v);
-
+        ancestors(ancestors, v);
         return ancestors;
+    }
+
+    private void ancestors(SET<Integer> ancestors, int w) {
+        if(ancestors.contains(w)) return;
+        Iterable<Integer> adjs = source.adj(w);
+        for (int v : adjs) {
+            ancestors.add(v);
+            ancestors(ancestors, v);
+        }
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        int result = -1;
+        int dist = Integer.MAX_VALUE;
+        for (int v1 : v)
+            for (int w1 : w) {
+                int candidate = ancestor(v1, w1);
+                if (dist < shortenPathes[v1].distTo(candidate) + shortenPathes[w1].distTo(candidate)) {
+                    dist = shortenPathes[v1].distTo(candidate) + shortenPathes[w1].distTo(candidate);
+                    result = dist;
+                }
+            }
+
+        return result;
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        int result = -1;
+        int dist = Integer.MAX_VALUE;
+        for (int v1 : v)
+            for (int w1 : w) {
+                int candidate = ancestor(v1, w1);
+                if (dist < shortenPathes[v1].distTo(candidate) + shortenPathes[w1].distTo(candidate)) {
+                    dist = shortenPathes[v1].distTo(candidate) + shortenPathes[w1].distTo(candidate);
+                    result = candidate;
+                }
+            }
+
+        return result;
     }
 
     // do unit testing of this class
