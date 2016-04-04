4d559ae5487de4ad2195c3d9a979af28cb2f634b
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-04-04 15:00:13 +0900

    cache result of computed energies

--- src/seamcarving/SeamCarver.java
+++ src/seamcarving/SeamCarver.java
@@ -4,10 +4,15 @@ import java.awt.Color;
 
 public class SeamCarver {
     private Picture source;
+    private double[][] energies;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
         source = new Picture(picture);
+        energies = new double[source.width()][source.height()];
+        for (int x = 0; x < source.width(); x++)
+            for (int y = 0; y < source.height(); y++)
+                energies[x][y] = getEnergy(x, y);
     }
 
     // current picture
@@ -27,6 +32,28 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
+        return energies[x][y];
+    }
+
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return new int[0];
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return new int[0];
+    }
+
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+    }
+
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+    }
+
+    private double getEnergy(int x, int y) {
         if (isBorder(x, y))
             return 1000;
 
@@ -48,24 +75,5 @@ public class SeamCarver {
 
     private boolean isBorder(int x, int y) {
         return x == width() - 1 || y == height() - 1 || x == 0 || y == 0;
-    }
-
-
-    // sequence of indices for horizontal seam
-    public int[] findHorizontalSeam() {
-        return new int[0];
-    }
-
-    // sequence of indices for vertical seam
-    public int[] findVerticalSeam() {
-        return new int[0];
-    }
-
-    // remove horizontal seam from current picture
-    public void removeHorizontalSeam(int[] seam) {
-    }
-
-    // remove vertical seam from current picture
-    public void removeVerticalSeam(int[] seam) {
     }
 }
