15dd15dff0522961242b6e98000cd8b4681fc374
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-04-04 11:02:31 +0900

    implement energy function

--- src/seamcarving/SeamCarver.java
+++ src/seamcarving/SeamCarver.java
@@ -1,5 +1,7 @@
 import edu.princeton.cs.algs4.Picture;
 
+import java.awt.Color;
+
 public class SeamCarver {
     private Picture source;
 
@@ -25,8 +27,31 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        return 0;
+        if (isBorder(x, y))
+            return 1000;
+
+        Color target = source.get(x, y);
+
+        Color left = source.get(x - 1, y);
+        Color right = source.get(x + 1, y);
+        Color top = source.get(x, y - 1);
+        Color bottom = source.get(x, y + 1);
+
+        int xGradientSquare = (right.getRed() - left.getRed()) * (right.getRed() - left.getRed()) +
+                (right.getGreen() - left.getGreen()) * (right.getGreen() - left.getGreen()) +
+                (right.getBlue() - left.getBlue()) * (right.getBlue() - left.getBlue());
+
+        int yGradientSquare = (top.getRed() - bottom.getRed()) * (top.getRed() - bottom.getRed()) +
+                (top.getGreen() - bottom.getGreen()) * (top.getGreen() - bottom.getGreen()) +
+                (top.getBlue() - bottom.getBlue()) * (top.getBlue() - bottom.getBlue());
+
+        return Math.sqrt(xGradientSquare + yGradientSquare);
     }
+
+    private boolean isBorder(int x, int y) {
+        return x - 1 == width() || y - 1 == height() || x == 0 || y == 0;
+    }
+
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
