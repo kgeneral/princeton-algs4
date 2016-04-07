91a2736b8fe6789a213c0919c5aff5b2dc1b87ef
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-04-07 15:11:14 +0900

    describe method definition

--- src/wordnet/Outcast.java
+++ src/wordnet/Outcast.java
@@ -1,10 +1,20 @@
 public class Outcast {
+
+    private WordNet wordnet;
+
     // constructor takes a WordNet object
     public Outcast(WordNet wordnet) {
+        this.wordnet = wordnet;
     }
 
     // given an array of WordNet nouns, return an outcast
+    // Given a list of wordnet nouns A1, A2, ..., An, which noun is the least related to the others? To identify an
+    // outcast, compute the sum of the distances between each noun and every other one:
+    // di = dist(Ai, A1) + dist(Ai, A2) + ... + dist(Ai, An)
+    // and return a noun At for which dt is maximum.
     public String outcast(String[] nouns) {
+
+
         return null;
     }
 
