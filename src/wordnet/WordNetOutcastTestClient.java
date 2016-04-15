d6a459ce3a309d08a54e537becb1f8fb5c3f54dd
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2016-04-15 17:03:22 +0900

    add test code

--- src/wordnet/WordNetOutcastTestClient.java
+++ src/wordnet/WordNetOutcastTestClient.java
@@ -4,6 +4,13 @@ import edu.princeton.cs.algs4.StdOut;
 public class WordNetOutcastTestClient {
     public static void main(String[] args) {
         WordNet wordnet = new WordNet(args[0], args[1]);
+
+//        for(String noun : wordnet.nouns())
+//            StdOut.print(noun + ", ");
+//        StdOut.println();
+
+        StdOut.println(wordnet.distance("change", "thing"));
+
         Outcast outcast = new Outcast(wordnet);
         for (int t = 2; t < args.length; t++) {
             In in = new In(args[t]);
--- src/wordnet/WordNetSAPTestClient.java
+++ src/wordnet/WordNetSAPTestClient.java
@@ -1,18 +1,28 @@
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class WordNetSAPTestClient {
     public static void main(String[] args) {
         In in = new In(args[0]);
         Digraph G = new Digraph(in);
         SAP sap = new SAP(G);
-        while (!StdIn.isEmpty()) {
-            int v = StdIn.readInt();
-            int w = StdIn.readInt();
+//        while (!StdIn.isEmpty()) {
+//            int v = StdIn.readInt();
+//            int w = StdIn.readInt();
+//            int length = sap.length(v, w);
+//            int ancestor = sap.ancestor(v, w);
+//            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+//        }
+        int n = 0;
+        while (n++ < 50000000) {
+
+            int v = StdRandom.uniform(G.V());
+            int w = StdRandom.uniform(G.V());
             int length = sap.length(v, w);
             int ancestor = sap.ancestor(v, w);
+            StdOut.printf("v = %d, w = %d\n", v, w);
             StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
         }
     }
