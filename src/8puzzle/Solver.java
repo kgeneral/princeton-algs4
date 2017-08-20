7a7e2dd35ff90bde8b27df17952a6c318b8df3bf
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2017-08-20 19:42:53 +0900

    feature branch - 8puzzle

--- src/8puzzle/Solver.java
+++ src/8puzzle/Solver.java
@@ -3,7 +3,11 @@ import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /*
 We define a search node of the game to be a board, the number of moves made to reach the board, and the previous search node.
@@ -15,115 +19,135 @@ The success of this approach hinges on the choice of priority function for a sea
 * */
 public class Solver {
 
-    private MinPQ<SolvableBoard> minPQ;
-    private int moves = 0;
-    private List<Board> solution;
+	private MinPQ<SolvableBoard> minPQ;
+	private int moves = 0;
+	private List<Board> solution;
 
-    // find a solution to the initial board (using the A* algorithm)
-    // TODO : remove board if another optimal path finded
-    public Solver(Board initial) {
-        solution = new ArrayList<>();
-        minPQ = new MinPQ<>();
+	// find a solution to the initial board (using the A* algorithm)
+	// TODO : remove board if another optimal path finded
+	public Solver(Board initial) {
+		Set<SolvableBoard> costSoFar = new HashSet<>();
+		Map<Board, Board> cameFrom = new HashMap<>();
+		solution = new ArrayList<>();
+		minPQ = new MinPQ<>();
 
-        minPQ.insert(new SolvableBoard(initial, moves));
-        Board dequeued = initial;
-        while (!minPQ.isEmpty()) {
+		minPQ.insert(new SolvableBoard(initial, moves));
+		Board goal = null;
+		while (!minPQ.isEmpty()) {
+			SolvableBoard current = minPQ.delMin();
+			// int costSoFar = current.priority();
+			// solution.add(dequeued);
+			if (current.isGoal()) {
+				goal = current.getBoard();
+				break;
+			}
+			moves = current.getMoved() + 1;
+			for (Board neighbor : current.getBoard().neighbors()) {
+				SolvableBoard neighborBoard = new SolvableBoard(neighbor, moves);
+				int newCost = neighborBoard.priority();
 
-            solution.add(dequeued);
-            if (dequeued.isGoal()) break;
-            moves++;
-            for (Board neighbor : dequeued.neighbors()) {
-                if (!solution.contains(neighbor))
-                    minPQ.insert(new SolvableBoard(neighbor, moves));
-            }
-            dequeued = minPQ.delMin().getBoard();
+				if (!costSoFar.contains(neighborBoard) || newCost < neighborBoard.priority()) {
+					costSoFar.add(neighborBoard);
 
-        }
+					minPQ.insert(neighborBoard);
+					// cameFrom.put(key, value)
+				}
 
-    }
+			}
 
-    // is the initial board solvable?
-    public boolean isSolvable() {
-        return moves != -1;
-    }
+		}
+		
+		if(goal == null)
+			moves = -1;
+		
 
-    // min number of moves to solve initial board; -1 if unsolvable
-    public int moves() {
-        return moves;
-    }
+	}
 
-    // sequence of boards in a shortest solution; null if unsolvable
-    public Iterable<Board> solution() {
-        return solution;
-    }
+	// is the initial board solvable?
+	public boolean isSolvable() {
+		return moves != -1;
+	}
 
-    // solve a slider puzzle (given below)
-    public static void main(String[] args) {
-        // create initial board from file
-        In in = new In(args[0]);
-        int N = in.readInt();
-        int[][] blocks = new int[N][N];
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                blocks[i][j] = in.readInt();
-        Board initial = new Board(blocks);
+	// min number of moves to solve initial board; -1 if unsolvable
+	public int moves() {
+		return moves;
+	}
 
-        // solve the puzzle
-        Solver solver = new Solver(initial);
+	// sequence of boards in a shortest solution; null if unsolvable
+	public Iterable<Board> solution() {
+		return solution;
+	}
 
-        // print solution to standard output
-        if (!solver.isSolvable())
-            StdOut.println("No solution possible");
-        else {
-            StdOut.println("Minimum number of moves = " + solver.moves());
-            for (Board board : solver.solution())
-                StdOut.println(board);
-        }
-    }
+	// solve a slider puzzle (given below)
+	public static void main(String[] args) {
+		// create initial board from file
+		In in = new In(args[0]);
+		int N = in.readInt();
+		int[][] blocks = new int[N][N];
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				blocks[i][j] = in.readInt();
+		Board initial = new Board(blocks);
 
+		// solve the puzzle
+		Solver solver = new Solver(initial);
 
-    private class SolvableBoard implements Comparable<SolvableBoard> {
-        private Board board;
-        private int moved;
+		// print solution to standard output
+		if (!solver.isSolvable())
+			StdOut.println("No solution possible");
+		else {
+			StdOut.println("Minimum number of moves = " + solver.moves());
+			for (Board board : solver.solution())
+				StdOut.println(board);
+		}
+	}
 
-        public SolvableBoard(Board board, int moved) {
-            this.board = board;
-            this.moved = moved;
-        }
+	private class SolvableBoard implements Comparable<SolvableBoard> {
+		private Board board;
+		private int moved;
 
-        public int priority() {
-            return board.manhattan() + moved;
-        }
+		public SolvableBoard(Board board, int moved) {
+			this.board = board;
+			this.moved = moved;
+		}
 
-        // TODO : tune priority condition
-        @Override
-        public int compareTo(SolvableBoard that) {
-            if (this.moved == that.moved) {
+		public int getMoved() {
+			return moved;
+		}
 
-                if (this.priority() == that.priority()) {
-                    if (this.board.hamming() == that.board.hamming())
-                        return 0;
+		public int priority() {
+			return board.manhattan() + moved;
+		}
 
-                    return (this.board.hamming() > that.board.hamming()) ? -1 : 1;
-                }
+		// TODO : tune priority condition
+		@Override
+		public int compareTo(SolvableBoard that) {
+			if (this.moved == that.moved) {
 
-                return (this.priority() < that.priority()) ? -1 : 1;
-            }
-            if (this.moved < that.moved)
-                return 1;
-            return -1;
-        }
+				if (this.priority() == that.priority()) {
+					if (this.board.hamming() == that.board.hamming())
+						return 0;
 
-        public boolean isGoal() {
-            return board.isGoal();
-        }
+					return (this.board.hamming() > that.board.hamming()) ? -1 : 1;
+				}
 
-        public Iterable<Board> neighbors() {
-            return board.neighbors();
-        }
+				return (this.priority() < that.priority()) ? -1 : 1;
+			}
+			if (this.moved < that.moved)
+				return 1;
+			return -1;
+		}
 
-        public Board getBoard() {
-            return board;
-        }
-    }
+		public boolean isGoal() {
+			return board.isGoal();
+		}
+
+		public Iterable<Board> neighbors() {
+			return board.neighbors();
+		}
+
+		public Board getBoard() {
+			return board;
+		}
+	}
 }
